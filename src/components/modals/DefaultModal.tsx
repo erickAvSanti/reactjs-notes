4cd5a0736ef9db875bc2f2428276ea06683a0b25
Erick Avalos Santiago <erick.avalos@rextie.com>
2025-01-07 11:33:08 -0500

    feat(components): refactor code

--- src/components/modals/DefaultModal.tsx
+++ src/components/modals/DefaultModal.tsx
@@ -53,25 +53,27 @@ export default function DefaultModal({
       onClick={closeBackdrop}
       ref={backdropRef}
     >
-      <div
-        className="flex flex-col items-center justify-center gap-2 bg-white dark:bg-gray-700 rounded-2xl p-3 min-w-[350px] shadow-2xl"
-        aria-modal="true"
-        role="dialog"
-        ref={modalRef}
-      >
-        <div className="flex flex-row items-center gap-2 w-full justify-between">
-          <span className="">{modalTitle ?? "Modal"}</span>
-          <button
-            className="p-1"
-            onClick={closeModal}
-            tabIndex={0}
-            ref={closeBtnRef}
-            aria-label="Close modal"
-          >
-            X
-          </button>
+      <div className="w-auto max-h-screen overflow-y-auto">
+        <div
+          className="flex flex-col items-center justify-center gap-2 bg-white dark:bg-gray-700 rounded-2xl p-3 min-w-[350px] shadow-2xl"
+          aria-modal="true"
+          role="dialog"
+          ref={modalRef}
+        >
+          <div className="flex flex-row items-center gap-2 w-full justify-between">
+            <span className="">{modalTitle ?? "Modal"}</span>
+            <button
+              className="py-1 px-2"
+              onClick={closeModal}
+              tabIndex={0}
+              ref={closeBtnRef}
+              aria-label="Close modal"
+            >
+              X
+            </button>
+          </div>
+          <div>{children}</div>
         </div>
-        <div>{children}</div>
       </div>
     </div>
   );
